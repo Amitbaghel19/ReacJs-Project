f3fe4fa571b3d2f2cf029f050120da0facc568e3
Amit Baghel <amit2003baghel@gmail.com>
2024-10-20 13:42:36 +0530

    second commit

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,18 +1,23 @@
 import React, { useState } from 'react'
 
 function App() {
-    const [num, changeNum] = useState(0)
+    const [num, changeNum] = useState(0);
+    const add=()=>{
+      changeNum(num+1);
+    }
+    const sub=()=>{
+      changeNum(num-1);
+      if(num<1){
+        alert("can't decrease")
+        changeNum(0);
+      }
+    }
   return (
     <div>
-         <button onClick={()=> changeNum(num+1)} id='plus'>+</button>
-      <input type='text' id='input-field' value={num}></input>
-      <button onClick={()=> {changeNum(num-1)
-        if(num<1){
-          alert("can't decreement")
-          changeNum(0)
-        }
-      
-      }} id='minus'>-</button>
+         <button onClick={add}>+</button>
+      <input value={num}></input>
+      <button onClick={sub}>-</button>
+       
     </div>
   )
 }
